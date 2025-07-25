35c1d50ff6d1f5bdde9484a9abb0f3777a13e251
Fernando <fernandocancino2004@gmail.com>
2025-07-25 10:06:33 -0600

    v11

--- resources/views/publicacione/form.blade.php
+++ resources/views/publicacione/form.blade.php
@@ -3,13 +3,13 @@
         
         <div class="form-group mb-2 mb20">
             <label for="titulo" class="form-label">Nombre del Servicio</label>
-            <input type="text" name="titulo" class="form-control @error('titulo') is-invalid @enderror" value="{{ old('titulo', $publicacione?->titulo) }}" id="titulo" placeholder="Ej: Reparación de computadoras, Clases de guitarra...">
+            <input type="text" name="titulo" class="form-control @error('titulo') is-invalid @enderror" value="{{ old('titulo', $publicacione?->titulo) }}" id="titulo" placeholder="">
             {!! $errors->first('titulo', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
         
         <div class="form-group mb-2 mb20">
             <label for="contenido" class="form-label">Descripción del Servicio</label>
-            <textarea name="contenido" class="form-control @error('contenido') is-invalid @enderror" id="contenido" rows="5" placeholder="Describe tu servicio, experiencia, precios, horarios disponibles...">{{ old('contenido', $publicacione?->contenido) }}</textarea>
+            <textarea name="contenido" class="form-control @error('contenido') is-invalid @enderror" id="contenido" rows="5" placeholder="Describe tu servicioio">{{ old('contenido', $publicacione?->contenido) }}</textarea>
             {!! $errors->first('contenido', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
 
